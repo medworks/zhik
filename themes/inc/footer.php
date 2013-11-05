b75c36cbe7851f7633f31c0ad88e1a4eca76f348
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-11-05 11:05:10 +0330

    change folder

---
+++ themes/inc/footer.php
@@ -0,0 +1,136 @@
+<!-- Footer Content -->
+<div id="footer-content-container">
+    <div id="footer-content-inner-wrapper" class="content-width">
+        <div id="footer-content" class="row top-margin">
+            <div class="large-4 columns less-padding" style="text-align:right;">
+                <img id="footer-logo" src="themes/images/logo.png" alt="">
+                <p>
+                    درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... درباره ژیک... 
+                </p>
+            </div>
+            <div class="large-4 columns less-padding">
+                <h5>اخبار</h5>
+                <p>
+                    سه خبر آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه خبر آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه خبر آخر در این قسمت قرار میگیرد 
+                </p>
+                <!-- <ul>
+                    <li>
+                        Phone: +123.456.789
+                    </li>
+                    <li>
+                        Mobile: +123.999.899
+                    </li>
+                    <li>
+                        Email: <a href="#"><span class="__cf_email__" data-cfemail="583b37362c393b2c18392a3b302c3d33763b3735">[email&nbsp;protected]</span><script type="text/javascript">
+/* <![CDATA[ */
+(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})();
+/* ]]> */
+</script></a>
+                    </li>
+                </ul> -->
+            </div>
+            <div class="large-4 columns less-padding">
+                <h5>پروژه ها</h5>
+                <p>
+                    سه پروژه آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه پروژه آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه پروژه آخر در این قسمت قرار میگیرد 
+                </p>
+                <!-- <a href="#" class="flat button">See Career Opportunity</a> -->
+            </div>
+            <div class="large-4 columns less-padding">
+                <h5>مقالات</h5>
+                <p>
+                    سه مقاله آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه مقاله آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه مقاله آخر در این قسمت قرار میگیرد 
+                </p>
+                <p>
+                    سه مقاله آخر در این قسمت قرار میگیرد 
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End id="footer-content-container" -->
+<!-- Footer Bar -->
+<div id="footer-bar-container" class="row">
+    <div id="footer-bar-inner-wrapper" class="content-width">
+        <div class="large-6 columns less-padding">
+            <div id="footer-social">
+                <ul class="bar-social">
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/facebook-bw.png" alt="" title=""><img class="hover" src="themes/images/social/facebook.png" alt="Facebook" title="Facebook"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/twitter-bw.png" alt="" title=""><img class="hover" src="themes/images/social/twitter.png" alt="Twitter" title="Twitter"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/googleplus-bw.png" alt="" title=""><img class="hover" src="themes/images/social/googleplus.png" alt="Google+" title="Google+"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/linkedin-bw.png" alt="" title=""><img class="hover" src="themes/images/social/linkedin.png" alt="LinkedIn" title="LinkedIn"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/flickr-bw.png" alt="" title=""><img class="hover" src="themes/images/social/flickr.png" alt="Flickr" title="Flickr"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/vimeo-bw.png" alt="" title=""><img class="hover" src="themes/images/social/vimeo.png" alt="Vimeo" title="Vimeo"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/youtube-bw.png" alt="" title=""><img class="hover" src="themes/images/social/youtube.png" alt="YouTube" title="YouTube"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/forrst-bw.png" alt="" title=""><img class="hover" src="themes/images/social/forrst.png" alt="Forrst" title="Forrst"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/dribbble-bw.png" alt="" title=""><img class="hover" src="themes/images/social/dribbble.png" alt="Dribbble" title="Dribbble"></a>
+                    </li>
+                    <li>
+                        <a href="#" target="_blank"><img src="themes/images/social/rss-bw.png" alt="" title=""><img class="hover" src="themes/images/social/rss.png" alt="RSS" title="RSS"></a>
+                    </li>
+                </ul>
+                <span>:ارتباط با ما از طریق</span>
+            </div>
+        </div>
+        <div class="large-6 columns less-padding">
+            Zhik © Copyright 2013, All rights reserved by Zhik company.
+        </div>
+    </div>
+</div> 
+<!-- End id="footer-bar-container" -->
+    <script src="themes/js/jquery-1.9.1.min.js"></script>
+    <script src="themes/js/jquery-ui-1.10.2.custom.min.js"></script>
+    <script src="themes/js/foundation.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=false&amp;v=3.5"></script>
+    <script src="themes/js/jquery.backstretch.min.js"></script>
+    <script src="themes/js/superfish.js"></script>
+    <script src="themes/js/supersubs.js"></script>
+    <script src="themes/js/jquery.hoverIntent.minified.js"></script>
+    <script src="themes/js/jquery.fancybox-1.3.4.js"></script>
+    <script src="themes/js/jquery.transit.min.js"></script>
+    <script src="themes/js/jquery.touchSwipe.min.js"></script>
+    <script src="themes/js/jquery.carouFredSel-6.1.0-packed.js"></script>
+    <script src="themes/js/jquery.easing.1.3.js"></script>
+    <script src="themes/js/jquery.isotope.min.js"></script>
+    <script src="themes/js/jquery.hoverdir.js"></script>
+    <script src="themes/js/jquery.validationEngine-en.js"></script>
+    <script src="themes/js/jquery.validationEngine.js"></script>
+    <script src="themes/js/jquery.scrollUp.min.js"></script>
+    <script src="themes/js/archtek.js?v=1"></script>
+</body>
+</html>
---
+++ themes/inc/header.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="fa"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="fa"><!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+    Remove this if you use the .htaccess -->
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+
+    <title>Zhik</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+
+    <!-- <script type="text/javascript">
+    //<![CDATA[
+    window.__CF=window.__CF||{};window.__CF.AJS={"ga_key":{"ua":"UA-34324154-1","ga_bs":"2"}};
+    //]]>
+    </script>
+    <script type="text/javascript">
+    //<![CDATA[
+    try{if (!window.CloudFlare) { var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/abv=2980380653/"},atok:"5126430ea0d4dc56c780be1507b53c46",petok:"7d8b81485ba067043daaca40f112e1e8-1383400502-1800",zone:"uxbarn.com",rocket:"0",apps:{"ga_key":{"ua":"UA-34324154-1","ga_bs":"2"}}}];var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.async=!0;a.src="//ajax.cloudflare.com/cdn-cgi/nexp/abv=616370820/cloudflare.min.js";b.parentNode.insertBefore(a,b);}}catch(e){};
+    //]]>
+    </script> -->
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700|Titillium+Web:400,600" rel="stylesheet" type="text/css">
+    <link href="./themes/css/reset.css" rel="stylesheet">
+    <link href="themes/css/foundation.css" rel="stylesheet">
+    <link href="themes/css/font-awesome.min.css" rel="stylesheet">
+    <link href="themes/css/isotope.css" rel="stylesheet">
+    <link href="themes/css/jquery.fancybox-1.3.4.css" rel="stylesheet">
+    <link href="themes/css/validationEngine.jquery.css" rel="stylesheet">
+    <link href="themes/css/archtek-init.css" rel="stylesheet">
+    <!--[if IE 8]>
+        <link href="themes/css/foundation-ie8.css" rel="stylesheet">
+        <link href="themes/css/archtek-ie8.css" rel="stylesheet">
+    <![endif]-->
+
+    <script src="themes/js/custom.modernizr.js"></script>
+
+    <link rel="shortcut icon" href="../favicon.ico">
+    <link rel="apple-touch-icon" href="../favicon.ico">
+
+    <!-- <script type="text/javascript">
+        /* <![CDATA[ */
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-34324154-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+        (function(b){(function(a){"__CF"in b&&"DJS"in b.__CF?b.__CF.DJS.push(a):"addEventListener"in b?b.addEventListener("load",a,!1):b.attachEvent("onload",a)})(function(){"FB"in b&&"Event"in FB&&"subscribe"in FB.Event&&(FB.Event.subscribe("edge.create",function(a){_gaq.push(["_trackSocial","facebook","like",a])}),FB.Event.subscribe("edge.remove",function(a){_gaq.push(["_trackSocial","facebook","unlike",a])}),FB.Event.subscribe("message.send",function(a){_gaq.push(["_trackSocial","facebook","send",a])}));"twttr"in b&&"events"in twttr&&"bind"in twttr.events&&twttr.events.bind("tweet",function(a){if(a){var b;if(a.target&&a.target.nodeName=="IFRAME")a:{if(a=a.target.src){a=a.split("#")[0].match(/[^?=&]+=([^&]*)?/g);b=0;for(var c;c=a[b];++b)if(c.indexOf("url")===0){b=unescape(c.split("=")[1]);break a}}b=void 0}_gaq.push(["_trackSocial","twitter","tweet",b])}})})})(window);
+        /* ]]> */
+    </script> -->
+</head>
+<body>
+    <script type="text/javascript" src="themes/js/CFInstall.js"></script>
+    <script>
+       CFInstall.check({
+            url: "./ie.html",
+            mode: "overlay",
+            destination: "http://www.mediateq.ir/zhik",
+       });
+    </script>
+<div id="header-container" class="content-width">
+    <!-- Logo -->
+    <div id="logo-wrapper">
+        <div id="logo">
+            <a href="./"><img src="themes/images/logo.png" alt="Zhik"></a>
+            <p class="ltr">Zhik.pro</p>
+        </div>
+    </div>
+    <!-- Menu -->
+    <div id="menu-wrapper">
+        <ul id="root-menu" class="sf-menu">
+            <li>
+                <a href="#" class="active">صفحه اصلی</a>
+                <ul>
+                    <li>
+                        <a href="index.php">صفحه اصلی 1</a>
+                    </li>
+                    <li>
+                        <a href="index-2.php">صفحه اصلی 2</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="themes/about.php">درباره ما</a>
+            </li>
+            <li>
+                <a href="#">پروژه ها</a>
+                <!-- <ul>
+                    <li>
+                        <a href="portfolio-list.php">List Page</a>
+                    </li>
+                    <li>
+                        <a href="portfolio-single.php">Single Page</a>
+                        <ul>
+                            <li>
+                                <a href="portfolio-single.php">Image/Slideshow Format</a>
+                            </li>
+                            <li>
+                                <a href="portfolio-single-video.php">Video Format</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul> -->
+            </li>
+            <li>
+                <a href="#">اخبار</a>
+                <!-- <ul>
+                    <li>
+                        <a href="blog-list.php">List Page</a>
+                    </li>
+                    <li>
+                        <a href="blog-list-left-sidebar.php">List Page (Left Sidebar)</a>
+                    </li>
+                    <li>
+                        <a href="blog-single.php">Single Page</a>
+                    </li>
+                </ul> -->
+            </li>
+            <li>
+                <a href="#">مقالات</a>
+                <!-- <ul>
+                    <li>
+                        <a href="gallery-1.php">Gallery Style 1</a>
+                    </li>
+                    <li>
+                        <a href="gallery-2.php">Gallery Style 2</a>
+                    </li>
+                    <li>
+                        <a href="team.php">Our Team</a>
+                    </li>
+                    <li>
+                        <a href="team-member-single.php">Team Member Profile</a>
+                    </li>
+                    <li>
+                        <a href="faq.php">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="404.php">404 - Page Not Found</a>
+                    </li>
+                </ul> -->
+            </li>
+            <li>
+                <a href="#">تماس با ما</a>
+                <!-- <ul class="flip">
+                    <li>
+                        <a href="elements-layouts.php">Layouts</a>
+                    </li>
+                    <li>
+                        <a href="elements-buttons.php">Buttons</a>
+                    </li>
+                    <li>
+                        <a href="elements-headings.php">Headings</a>
+                    </li>
+                    <li>
+                        <a href="elements-icons.php">Icons</a>
+                    </li>
+                    <li>
+                        <a href="elements-images-videos.php">Images and Videos</a>
+                    </li>
+                    <li>
+                        <a href="elements-blockquotes.php">Blockquotes</a>
+                    </li>
+                    <li>
+                        <a href="elements-tabs-message-boxes.php">Tabs and Message Boxes</a>
+                    </li>
+                    <li>
+                        <a href="elements-accordion-toggles.php">Accordion and Toggles</a>
+                    </li>
+                    <li>
+                        <a href="elements-google-maps.php">Google Maps</a>
+                    </li>
+                    <li>
+                        <a href="elements-testimonial-slider.php">Testimonial Slider</a>
+                    </li>
+                    <li>
+                        <a href="elements-dropcaps-highlights-dividers.php">Drop Caps, Highlights and Dividers</a>
+                    </li>
+                </ul> -->
+            </li>
+            <!-- <li>
+                <a href="contact-us.php">Contact</a>
+            </li> -->
+        </ul>
+        <nav id="mobile-menu" class="top-bar">
+            <ul class="title-area">
+                <!-- Do not remove this list item -->
+                <li class="name"></li>
+                
+                <!-- Menu toggle button -->
+                <li class="toggle-topbar menu-icon">
+                    <a href="#"><span></span></a>
+                </li>
+            </ul>
+            
+            <!-- Mobile menu's container -->
+            <section class="top-bar-section"></section>
+        </nav>
+    </div>
+    <!-- Search -->
+    <div id="header-search">
+        <a id="header-search-button" href="javascript:;"><i class="icon-search"></i></a>
+    </div>
+    <div id="header-search-input-wrapper">
+        <form method="get">
+            <input id="header-search-input" type="text" placeholder="Type and hit enter to search ...">
+        </form>
+    </div>
+</div>
+<!-- End id="header-container" -->

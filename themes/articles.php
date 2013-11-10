b13273a1e24a5b9869e2a0aaa39de629be9315b2
Saeid Hatami <hatami4510@gmail.com>
2013-11-10 10:29:33 +0330

    compeleting articles page

--- themes/articles.php
+++ themes/articles.php
@@ -1,6 +1,16 @@
 <?php
-
-$html=<<<cd
+  include_once("./classes/database.php");
+  include_once("./lib/persiandate.php");
+  $db = Database::GetDatabase();
+  $pageNo = ($_GET["pid"]) ? $_GET["pid"] : 1;
+  $maxItemsInPage = GetSettingValue('Max_Post_Number',0);
+  $from = ($pageNo - 1) * $maxItemsInPage;
+  $count = $maxItemsInPage;
+  
+  $articles = $db->SelectAll("articles","*",null,"ndate DESC",$from,$count);  
+  $itemsCount = $db->CountAll("articles");
+     
+  $html=<<<cd
     <div id="header-image-container">
         <div id="header-image">
             <img src="themes/images/demo/blog/blog-header.jpg" alt="" class="stretch-image">
@@ -13,7 +23,7 @@ $html=<<<cd
                 <span>مسیر شما:</span>
                 <ul class="breadcrumbs">
                     <li class="current">
-                        <a>مقالات</a>
+                        <a>اخبار</a>
                     </li>
                     <li>
                         <a href="./">صفحه اصلی</a>
@@ -25,19 +35,27 @@ $html=<<<cd
         <!-- Page Intro -->
         <div id="intro" class="not-homepage row">
             <div class="large-9 large-centered columns">
-                <h1>مقالات <strong>ژیک</strong></h1>
+                <h1>اخبار <strong>ژیک</strong></h1>
             </div>
         </div>
-        <!-- Blog List -->
+		<!-- Blog List -->
         <div class="grey-bg row">
             <div class="large-9 columns for-nested">
-                <div class="blog-item row">
+cd;
+foreach($news as $key => $post)
+{
+	$ndate = ToJalali($post["ndate"]," l d F  Y ساعت H:m");
+  	$post["userid"] = GetUserName($post["userid"]);	
+    $post["body"]= strip_tags($post["body"]);
+    $post["body"] = (mb_strlen($post["body"])>500) ? mb_substr($post["body"],0,500,"UTF-8")."..." : $post["body"];
+$html.=<<<cd
+                 <div class="blog-item row">
                     <div class="large-12 columns height-255">
                         <div class="blog-meta">
                             <span class="date">1392/08/15</span>
                             <ul class="author-comments">
                                 <li>
-                                    <a href="#">مجتبی امجدی</a><i class="icon-user"></i>
+                                    <a href="#">{$post["userid"]}</a><i class="icon-user"></i>
                                 </li>
                                 <!-- <li>
                                     <a href="#">5 Comments</a><i class="icon-comments-alt"></i>
@@ -45,95 +63,24 @@ $html=<<<cd
                             </ul>
                         </div>
                         <hr>
-                        <h2 class="blog-title"><a href="blog-single.php">خبر اول</a></h2>
+                        <h2 class="blog-title"><a href="news-fullpage{$post[id]}.html">{$post["subject"]}</a></h2>
                         <p class="excerpt">
-                            توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... 
+                            {$post["body"]}
                         </p>
-                        <a href="#" class="small flat radius button">ادامه مقاله</a>
+                        <a href="news-fullpage{$post[id]}.html" class="small flat radius button">ادامه خبر</a>
                     </div>
                     <div class="large-12 columns no-padding">
-                        <img src="themes/images/demo/blog/blog-item2.jpg" alt="">
+                        <img src="{$post[image]}" alt="{$post[subject]}" style="width:765px;height:255px;">
                     </div>
-                </div>
-                <div class="blog-item row">
-                    <div class="large-12 columns height-255">
-                        <div class="blog-meta">
-                            <span class="date">1392/08/15</span>
-                            <ul class="author-comments">
-                                <li>
-                                    <a href="#">مجتبی امجدی</a><i class="icon-user"></i>
-                                </li>
-                                <!-- <li>
-                                    <a href="#">5 Comments</a><i class="icon-comments-alt"></i>
-                                </li> -->
-                            </ul>
-                        </div>
-                        <hr>
-                        <h2 class="blog-title"><a href="blog-single.php">خبر اول</a></h2>
-                        <p class="excerpt">
-                            توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... 
-                        </p>
-                        <a href="#" class="small flat radius button">ادامه مقاله</a>
-                    </div>
-                    <div class="large-12 columns no-padding">
-                        <img src="themes/images/demo/blog/blog-item2.jpg" alt="">
-                    </div>
-                </div>
-                <div class="blog-item row">
-                    <div class="large-12 columns height-255">
-                        <div class="blog-meta">
-                            <span class="date">1392/08/15</span>
-                            <ul class="author-comments">
-                                <li>
-                                    <a href="#">مجتبی امجدی</a><i class="icon-user"></i>
-                                </li>
-                                <!-- <li>
-                                    <a href="#">5 Comments</a><i class="icon-comments-alt"></i>
-                                </li> -->
-                            </ul>
-                        </div>
-                        <hr>
-                        <h2 class="blog-title"><a href="blog-single.php">خبر اول</a></h2>
-                        <p class="excerpt">
-                            توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... توضیحات خبر اوب... 
-                        </p>
-                        <a href="#" class="small flat radius button">ادامه مقاله</a>
-                    </div>
-                    <div class="large-12 columns no-padding">
-                        <img src="themes/images/demo/blog/blog-item2.jpg" alt="">
-                    </div>
-                </div>
+                </div> 
+cd;
+}
+$linkFormat = 'news-page'.$pid='%PN%'.'.html';
+$maxPageNumberAtTime = GetSettingValue('Max_Page_Number',0);
+$pageNos = Pagination($itemsCount, $maxItemsInPage, $pageNo, $maxPageNumberAtTime, $linkFormat);
+$html .= '<center>' . $pageNos . '</center>';
 
-                <div class="row">
-                    <div id="blog-pagination" class="rtl large-12 columns pagination-centered">
-                        <ul class="pagination">
-                            <li class="unavailable">
-                                <a href="javascript:;"><i class="icon-angle-right"></i></a>
-                            </li>
-                            <li class="current">
-                                <a href="javascript:;">1</a>
-                            </li>
-                            <li>
-                                <a href="#">2</a>
-                            </li>
-                            <li>
-                                <a href="#">3</a>
-                            </li>
-                            <li class="unavailable">
-                                <a href="#">…</a>
-                            </li>
-                            <li>
-                                <a href="#">8</a>
-                            </li>
-                            <li>
-                                <a href="#">9</a>
-                            </li>
-                            <li class="arrow">
-                                <a href="#"><i class="icon-angle-left"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+$html.=<<<cd
             </div>
             <div id="sidebar-wrapper" class="large-3 columns for-nested">
                 <div class="widget-item row">
@@ -148,40 +95,35 @@ $html=<<<cd
                 </div>
                 <div class="widget-item row">
                     <div class="large-12 columns">
-                        <div class="posts-widget">
-                            <h4>آخرین مقالات</h4>
+					<div class="posts-widget">
+                            <h4>آخرین اخبار</h4>
                             <ul>
+cd;
+$posts = $db->SelectAll("news","*",null,"ndate DESC");
+for($i = 0;$i<7;$i++)
+{
+  if (!isset($posts[$i][id])) break;
+	$ndate = ToJalali($posts[$i]["ndate"]," l d F  Y");
+$html.=<<<cd
+                        
                                 <li>
                                     <div class="post-thumbnail">
-                                        <a href="blog-single.php"><img src="themes/images/demo/portfolio/project-thumb7.jpg" alt=""></a>
+                                        <a href="news-fullpage{$posts[$i][id]}.html">
+										<img src="{$posts[$i][image]}" alt="{$posts[$i][subject]}" style="width:50px;height:50px;"></a>
                                     </div>
                                     <div class="post-title">
-                                        <a href="blog-single.php">Sed ut perspicia unde omnis iste natus</a>
-                                        <span class="date">May 30, 2013</span>
+                                        <a href="news-fullpage{$posts[$i][id]}.html">{$posts[$i][subject]}</a>
+                                        <span class="date">{$ndate}</span>
                                     </div>
-                                </li>
-                                <li>
-                                    <div class="post-thumbnail">
-                                        <a href="blog-single.php"><img src="themes/images/demo/portfolio/project-thumb4.jpg" alt=""></a>
-                                    </div>
-                                    <div class="post-title">
-                                        <a href="blog-single.php">Lorem ipsum dolor ..</a>
-                                        <span class="date">May 15, 2013</span>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="post-thumbnail">
-                                        <a href="blog-single.php"><img src="themes/images/demo/portfolio/project-thumb1.jpg" alt=""></a>
-                                    </div>
-                                    <div class="post-title">
-                                        <a href="blog-single.php">Ut enim ad minim veniam consec ..</a>
-                                        <span class="date">May 9, 2013</span>
-                                    </div>
-                                </li>
-                            </ul>
+                                </li>                            
+cd;
+}
+$html.=<<<cd
+                  </ul>
                         </div>
                     </div>
                 </div>
+<!--				
                 <div class="widget-item row">
                     <div class="large-12 columns">
                         <div class="category-widget">
@@ -206,25 +148,7 @@ $html=<<<cd
                         </div>
                     </div>
                 </div>
-                
-           <!-- <div class="widget-item row">
-                    <div class="large-12 columns">
-                        <div class="flickr-widget">
-                            <h4>Flickr Gallery</h4>
-                            <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&amp;count=6&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=52617155@N08"></script>
-                        </div>
-                    </div>
-                </div> -->
-           <!-- <div class="widget-item row">
-                    <div class="large-12 columns">
-                        <div class="custom-text-widget">
-                            <h4>Custom Text</h4>
-                            <p>
-                                Ut enim ad minim veniam consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam consectetur adipisicing elit, sed do eiusmod
-                            </p>
-                        </div>
-                    </div>
-                </div> -->    
+-->				
             </div>
         </div>
     </div>

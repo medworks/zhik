e83c5291e7f98706fed528b396e5a82dc854c5ab
Saeid Hatami <hatami4510@gmail.com>
2013-11-11 17:45:36 +0330

    fix some errors for plan section

--- manager/worksmgr.php
+++ manager/worksmgr.php
@@ -256,15 +256,18 @@ if ($_GET['act']=="new" or $_GET['act']=="edit")
 		   </p>
 		   <textarea cols="50" rows="10" name="detail" class="detail" id="detail">{$row[body]}</textarea>
 		   <p>
-			 <label for='plan'>فایل پلان</label>
+			 <label for="pic">فایل پلان</label>
 			 <span>*</span>
 		   </p>
-		   <div class='upload-file'>
-				<input type='file' name='plan' class='validate[required] pic ltr' id='plan' value='{$row[plan]}' onChange=''/>
-				<span class='filename'>لطفا فابل مورد نظر را انتخاب کنید</span>
-				<span class='action'>انتخاب فایل</span>
-			</div>
-			<div class="badboy"></div>
+		   <p>
+		   		<input type="text" name="selectpic" class="selectpic" id="selectpic" value='{$row[image]}' />
+		   		<input type="text" class="validate[required] showadd" id="showadd" value='{$row[image]}' />
+		   		<a class="filebrowserbtn" id="planbrowserbtn" name="worksmgr" title="گالری تصاویر">گالری تصاویر</a>
+		   		<a class="selectbuttton" id="selectplanbuttton" title="انتخاب">انتخاب</a>
+		   </p>
+		   <div class="badboy"></div>
+		   <div id="planbrowser"></div>		   
+		   <div class="badboy"></div>
 		   <p>
 			 <label for='price'>فایل جدول قیمت</label>
 			 <span>*</span>

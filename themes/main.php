7990801a9bdf43b1000932dace9ffa27b6bbfc74
Saeid Hatami <hatami4510@gmail.com>
2013-11-14 02:00:22 -0800

    fix seo problems

--- themes/main.php
+++ themes/main.php
@@ -1,10 +1,9 @@
 <?php
-    // ob_start();
+    ob_start();
+	include_once("inc/index-content.php");
+	$page_content = ob_get_contents();
+	ob_end_clean();	
     include_once("inc/header.php");
-
-    include_once("inc/index-content.php");
-    // $page_content = ob_get_contents();
-    // ob_end_clean(); 
-    // echo $page_content;
+    echo $page_content;
     include_once("inc/footer.php");
 ?>

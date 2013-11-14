9f71772928df2627cb1a11dab3afadeab7ae19a3
Saeid Hatami <hatami4510@gmail.com>
2013-11-14 03:53:49 -0800

    add new setting table for adding to database

---
+++ settings.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2013 at 12:53 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zhik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(30) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `key`, `value`) VALUES
+(1, 'Site_Theme_Name', 'default'),
+(2, 'About_System', 'شرکت ساختمانی ژیک در سال 1390 به جهت ایجاد ساختمان های شیک جهت رفاه حال اقشار و اصناف مختلف ایجاد \nگردیده است ، لذا با توجه به گستردگی اصناف بازار ساختمان خصوصا جهت اقشار مهندسی و پزشکی سخت گردید هست . اما با این حال ژِک توانسته است رضایت خاطر اقسام مختلف سلایق را جلب کند'),
+(3, 'Site_Title', 'سیستم مدیریت محتوای مدیا تکنیک'),
+(4, 'Site_KeyWords', 'مدیا تک - سی ام اس - مدیریت محتوا'),
+(5, 'Site_Describtion', 'این سایت بر پایه phpطراحی شده است که باعث سهولت در'),
+(6, 'Admin_Email', 'admin@mediateq.ir'),
+(7, 'News_Email', 'news@mediateq.ir'),
+(8, 'Contact_Email', 'info@mediateq.ir'),
+(9, 'Max_Page_Number', '5'),
+(10, 'Max_Post_Number', '3'),
+(11, 'FaceBook_Add', 'facebook.com'),
+(12, 'Twitter_Add', 'twitter.com'),
+(13, 'Rss_Add', '127.0.01/media/rssfeed.php'),
+(14, 'YouTube_Add', 'youtube.com'),
+(15, 'Tell_Number', '7623666'),
+(16, 'Fax_Number', '7634562'),
+(17, 'Address', 'مشهد - سه راه فلسطین - ساختمان 202 - طبقه اول - واحد 3'),
+(18, 'Is_Smtp_Active', 'yes'),
+(19, 'Smtp_Host', 'smtp.gmail.com'),
+(20, 'Smtp_User_Name', 'hatami4510@gmail.com'),
+(21, 'Smtp_Pass_Word', '12345'),
+(22, 'Smtp_Port', '465'),
+(23, 'Email_Sender_Name', 'گروه مدیاتک'),
+(24, 'WellCome_Title', 'به سایت ژیک خوش آمدید '),
+(25, 'WellCome_Body', 'شرکت ساختمانی ژیک  جهت رفاه حال پزشکان ساختمانی شیک طراحی و ساخته است'),
+(26, 'Cell_Phone1', '0915124395'),
+(27, 'Cell_Phone2', '09151091162');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

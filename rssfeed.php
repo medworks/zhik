6190516c45802af0e46ece451471b76e710d2077
Saeid Hatami <hatami4510@gmail.com>
2013-11-14 03:01:42 -0800

    create rss feed to project

--- rssfeed.php
+++ rssfeed.php
@@ -10,13 +10,13 @@
 	$Site_Describtion=GetSettingValue("Site_Describtion",1);
 	$Admin_Email=GetSettingValue("Admin_Email",1);
 	$now = date("D, d M Y H:i:s T");
-	$site = "http://www.mediateq.ir";
+	$site = "http://www.zhiktower.com";
 	$uri = "?item=fullnews&act=do&wid=";
 	
 	$rss2_writer = new RSS2Writer(
 									$Site_Title, 
 									$Site_Describtion, 
-									'www.mediateq.ir', 
+									'www.zhiktower.com', 
 									6, //indent
 									false //use CDATA
 		                          );
